7cea1a62d7af6e7301f37826c5450eb4a2a9f6cf
unknown <erwin@gostyle.studio>
2019-09-03 16:55:49 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -13,9 +13,9 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['http://localhost:3000', 'http://ec2-3-15-169-11.us-east-2.compute.amazonaws.com:3000/'],
     'allowedOriginsPatterns' => [],
-    'allowedHeaders' => ['Content-Type'],
+    'allowedHeaders' => ['Content-Type','Authorization'],
     'allowedMethods' => ['GET','POST','PUT','DELETE'],
     'exposedHeaders' => [],
     'maxAge' => 0,
